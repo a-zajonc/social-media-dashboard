3f1bdc5198a4b94f593bf3c7f373578eb5f0d8e2
Adriana <97173869+a-zajonc@users.noreply.github.com>
2022-11-16 10:52:01 +0100

    Add Black Background and White Text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
 import { DashboardComponent } from "./components";
 import { theme } from ".";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <DashboardComponent />
+      <Box backgroundColor="#1E202A" h="100vh">
+        <DashboardComponent />
+      </Box>
     </ChakraProvider>
   );
 }
