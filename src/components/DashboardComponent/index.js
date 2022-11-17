7fe0635e45909c04b17d4a423886c018b5d76632
Adriana <97173869+a-zajonc@users.noreply.github.com>
2022-11-17 14:16:36 +0100

    Get Total Followers Number

--- src/components/DashboardComponent/index.js
+++ src/components/DashboardComponent/index.js
@@ -2,8 +2,16 @@ import { ThemeMode } from "./ThemeMode";
 import { Box, Text } from "@chakra-ui/react";
 import { SocialMediaDisplay } from "./SocialMediaDisplay";
 import { OverviewDisplay } from "./OverviewDisplay";
+import { useContext } from "react";
+import { UserData } from "../../context";
 
 export function DashboardComponent() {
+  const data = useContext(UserData);
+
+  const followersSum = data.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue.amount;
+  }, 0);
+
   return (
     <Box
       h="100%"
@@ -16,7 +24,9 @@ export function DashboardComponent() {
           <Text fontWeight="700" fontSize="24px" color="white">
             Social Media Dashboard
           </Text>
-          <Text color="#8B97C6">Total Followers:23,004</Text>
+          <Text color="#8B97C6" fontWeight="700">
+            Total Followers: {followersSum}
+          </Text>
         </Box>
         <ThemeMode />
       </Box>
