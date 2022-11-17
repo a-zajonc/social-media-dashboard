d62d4c762070c6046a5f425ea9748a457079b848
Adriana <97173869+a-zajonc@users.noreply.github.com>
2022-11-17 11:35:34 +0100

    Install Express and Server.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import * as React from "react";
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import { DashboardComponent } from "./components";
 import { theme } from ".";
 
+const getFeed = async () => {
+  const response = await fetch("http://localhost:3001/feed");
+
+  return response.json();
+};
+
 function App() {
+  const [feed, setFeed] = React.useState([]);
+
+  React.useEffect(() => {
+    const resolveFeed = async () => {
+      const data = await getFeed();
+
+      setFeed(data);
+    };
+    resolveFeed();
+  }, []);
+
+  console.log("dupa");
   return (
     <ChakraProvider theme={theme}>
       <Box
