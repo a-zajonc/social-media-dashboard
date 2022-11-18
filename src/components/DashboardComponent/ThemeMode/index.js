e623d6fc4abff33e779367c3442f504610718ac2
Adriana <97173869+a-zajonc@users.noreply.github.com>
2022-11-18 15:02:31 +0100

    Fix Toggle Theme Mode

--- src/components/DashboardComponent/ThemeMode/index.js
+++ src/components/DashboardComponent/ThemeMode/index.js
@@ -2,14 +2,18 @@ import { Text, Box, Switch } from "@chakra-ui/react";
 import { useColorMode } from "@chakra-ui/react";
 
 export function ThemeMode() {
-  const { toggleColorMode } = useColorMode();
-
+  const { colorMode, toggleColorMode } = useColorMode();
+  console.log(colorMode);
   return (
     <Box display="flex" flexDirection="row" alignItems="center">
       <Text fontSize="16px" color="#8B97C6" fontWeight="700" marginRight="10px">
         Dark Mode
       </Text>
-      <Switch size="lg" onChange={toggleColorMode} />
+      <Switch
+        size="lg"
+        onChange={toggleColorMode}
+        isChecked={colorMode === "light"}
+      />
     </Box>
   );
 }
